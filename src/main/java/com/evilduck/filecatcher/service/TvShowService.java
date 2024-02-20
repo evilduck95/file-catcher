749774a5a8596920773784682b3694d08da62979
evilduck95 <keiranbrown95@gmail.com>
2024-02-20 23:30:03 +0000

    Adding some per-episode parsing, started a Regex for recognising file name parts for an episode.

--- src/main/java/com/evilduck/filecatcher/service/TvShowService.java
+++ src/main/java/com/evilduck/filecatcher/service/TvShowService.java
@@ -1,6 +1,5 @@
 package com.evilduck.filecatcher.service;
 
-import com.evilduck.filecatcher.configuration.FileDefaults;
 import com.evilduck.filecatcher.exception.IncorrectFileFormatException;
 import com.evilduck.filecatcher.respository.FileRepository;
 import org.springframework.core.io.Resource;
@@ -10,10 +9,13 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URLConnection;
 import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TvShowService extends FileService {
 
+    private static final Pattern FILE_NAME_PATTERN = Pattern.compile("(.*)(s[0-9]{2})(e[0-9]{2}).*\\.(.+)");
     private final ZipManager zipManager;
     private final FileRepository tvShowRepository;
 
@@ -26,8 +28,29 @@ public class TvShowService extends FileService {
 
     private void parseTvShow(final File tempTvShowFolder) {
         // TODO: Parse tv show, seasons and episodes.
+        final File[] seasons = safeListDirectory(tempTvShowFolder);
+        for (File seasonFolder : seasons) {
+            final File[] episodes = safeListDirectory(seasonFolder);
+            for (File episodeFile : episodes) {
+                final String episodeNameRaw = episodeFile.getName();
+                final Matcher episodeNameMatcher = FILE_NAME_PATTERN.matcher(episodeNameRaw);
+                // TODO: This could be anything or nothing, usually a show name, might need modifying.
+                final String episodeName = episodeNameMatcher.group(1);
+                final String seasonNumber = episodeNameMatcher.group(2);
+                final String episodeNumber = episodeNameMatcher.group(3);
+                final String fileExtension = episodeNameMatcher.group(4);
+                final String outputFileName = String.format("%1$s_%2$s%3$s.%4$s", episodeName, seasonNumber, episodeNumber, fileExtension);
+
+            }
+        }
+
     }
 
+    private File[] safeListDirectory(final File directory) {
+        final File[] list = directory.listFiles();
+        if (list == null) throw new RuntimeException("Unable to list directory [" + directory.getPath() + ']');
+        return list;
+    }
 
     @Override
     public void save(final Resource media, final String contentType) throws IOException {
