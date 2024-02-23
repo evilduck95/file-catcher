a334f9aca1e2cc400ff9c5871894ebba5a3dad10
evilduck95 <keiranbrown95@gmail.com>
2024-02-23 21:28:25 +0000

    Silly dean not using the annotation :3

--- src/main/java/com/evilduck/filecatcher/model/Film.java
+++ src/main/java/com/evilduck/filecatcher/model/Film.java
@@ -3,16 +3,16 @@ package com.evilduck.filecatcher.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class Film extends Media {
 
     private int resolution;
     private Subtitles subtitles;
 
-    public Film() {
-
-    }
 }
+
