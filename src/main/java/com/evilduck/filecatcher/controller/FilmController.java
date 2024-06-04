3192b1e2450af17c6b662da295a6d80f93258817
evilduck95 <keiranbrown95@gmail.com>
2024-06-04 20:15:47 +0100

    Added some config for upload storage.

--- src/main/java/com/evilduck/filecatcher/controller/FilmController.java
+++ src/main/java/com/evilduck/filecatcher/controller/FilmController.java
@@ -3,12 +3,14 @@ package com.evilduck.filecatcher.controller;
 import com.evilduck.filecatcher.dto.FileUploadResponse;
 import com.evilduck.filecatcher.request.JobProcessingRequest;
 import com.evilduck.filecatcher.service.FilmService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Slf4j
 @RestController
 @RequestMapping("/film")
 @CrossOrigin(origins = {"http://localhost:3000", "https://evilduck95.net"})
@@ -22,6 +24,7 @@ public class FilmController {
 
     @PostMapping("/upload")
     public ResponseEntity<FileUploadResponse> uploadFilm(@RequestParam("file") final MultipartFile file) throws IOException {
+        log.info("Received upload of file [{}]", file.getName());
         final String jobId = filmService.save(file.getResource(), file.getContentType());
         final FileUploadResponse fileUploadResponse = new FileUploadResponse("success", jobId);
         return ResponseEntity.ok(fileUploadResponse);
